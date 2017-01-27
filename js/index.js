5225b558ca8269500c6a01928451f42bc83c61ae
JeanMary <jeanmary@gmail.com>
2017-01-27 01:26:35 +0100

    add a overflow on header carousel

--- js/index.js
+++ js/index.js
@@ -17,7 +17,15 @@ function animate_paragraphe(e)
   );
 
   setInterval(function() {
-    position += 1;
+    if(position == 2)
+    {
+      position = 0;
+    }
+    else
+    {
+      position += 1;
+    }
+
     $("#carousel").css('margin-left', (-1000*position) +'px')
   }, 8000)
 }
